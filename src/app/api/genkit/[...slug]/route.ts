6c2f98f0454f6bc86aab37c3d55597ceb25f90ec
Fuad modaresi rad <fuadmodaresirad@gmail.com>
2025-09-29 13:47:13 +0000

    TypeError: (0 , a.createApiHandler) is not a function

--- src/app/api/genkit/[...slug]/route.ts
+++
@@ -1,5 +0,0 @@
-import {createApiHandler} from '@genkit-ai/next';
-import '@/ai/flows/adaptive-learning-paths';
-import '@/ai/flows/virtual-teacher';
-
-export const {GET, POST} = createApiHandler();
